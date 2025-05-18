d5ab1f41036f44cb2ec27ee35b6c41bc94800c74
Volodymy Rudyk <rudykvova@gmail.com>
2025-05-18 15:01:18 +0300

    Setup UI

---
+++ PersonalQuize/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalQuize
+//
+//  Created by Володя Рудик on 18.05.2025.
+//
+
+import Foundation
---
+++ PersonalQuize/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuize
+//
+//  Created by Володя Рудик on 18.05.2025.
+//
+
+import UIKit
+
+final class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
